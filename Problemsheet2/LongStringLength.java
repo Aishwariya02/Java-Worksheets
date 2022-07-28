07ae4ab81400322195a6ab9e55d52ecef93f7074
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-07-28 14:58:20 +0530

    Add files via upload

---
+++ Problemsheet2/LongStringLength.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 20pt02
+ */
+public class LongStringLength 
+{
+    static int[] columnMax(String[][] a)
+    {
+        int cols = a[0].length;
+        int rows = a.length;
+        System.out.println(cols + " " + rows);
+        int arr[];
+        arr = new int[cols];
+        //arr = new int[cols];
+        String word;
+        
+        for(int i = 0;i<cols;i++)
+        {
+            int maxLength = 0;
+            for(int j = 0;j<rows;j++)
+            {
+                word = a[j][i];
+                if(word.length() > maxLength)
+                {
+                    maxLength = word.length();
+                }
+            }
+            arr[i] = maxLength;
+        }
+        return arr;
+    }
+    
+    public static void main(String[] args)
+    {
+        String[][] a = 
+        {{"help", "ok", "city lights","summer"},
+         {"elvisLives", "hello", "punch-out","pretty"},
+         {"hurrah", "humptydumpty", "123","brothers"}};
+        int[] arr = columnMax(a);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
