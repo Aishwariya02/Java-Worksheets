80dba5d23fca960948e46aa916588fa10f3d939f
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-07-28 22:19:24 +0530

    Create Q9.java

---
+++ Problemsheet1/Q9.java
@@ -0,0 +1,11 @@
+class Power{
+    public static void main(String[] args) {
+        System.out.println("A B POW(A,B)");
+        for(int i=1;i<=7;i++)
+        {
+            int j = i+1;
+            int val = (int)Math.pow(i,j);
+            System.out.println(i +" "+ j + " " + val);
+        }
+    }
+}
