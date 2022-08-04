7e274dcc1896dfbc31649d3cf9aa4f4386b2657a
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-08-04 15:10:56 +0530

    Create OrdersApplication.java

---
+++ Problemsheet3/OrdersApplication/OrdersApplication.java
@@ -0,0 +1,15 @@
+package ordersapplication;
+
+/**
+ *
+ * @author 20pt02
+ */
+public class OrdersApplication {
+
+    public static void main(String[] args) 
+    {
+        Orders O[];
+        
+    }
+    
+}
