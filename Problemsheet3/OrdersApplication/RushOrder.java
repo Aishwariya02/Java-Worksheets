14d3b27fe7146eb83ae8b4a627a18254d10813be
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-08-04 15:12:11 +0530

    Add files via upload

---
+++ Problemsheet3/OrdersApplication/RushOrder.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordersapplication;
+
+/**
+ *
+ * @author 20pt02
+ */
+import java.util.*;
+
+public class RushOrder extends Orders{
+    protected int deliveryDay;
+    public RushOrder()
+    {
+        super();
+    }
+    int getTotal()
+    {
+        int total = super.getTotal();
+        switch (deliveryDay) {
+            case 1:
+                total += 25;
+                break;
+            case 2:
+                total += 15;
+                break;
+            case 3:
+                total += 10;
+                break;
+            default:
+                total += 0;
+                break;
+        }
+        return total;
+    }
+    
+}
